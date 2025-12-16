3fd895f4c3ee6bea51b33f98a2041fe6b9d98872
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2025-12-17 01:38:41 +0530

    Home page skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
+import "./App.css"
+import Body from "./components/Body"
+import Head from "./components/Head"
+import { Provider } from "react-redux"
+import store from "./store/appStore"
 
 function App() {
 
+  {/** 
+    - Head
+    - Body
+        - Sidebar
+          - MenuItems
+        - MainContainer
+          - ButtonList
+          - VideoContainer
+            - VideoCard
+
+  */}
+
   return (
-    <>
-      <div>
-        <h1>Namaste React App</h1>
+    <Provider store={store}>
+      <div className="">
+        <Head />
+        <Body />
       </div>
-    </>
+    </Provider>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Sidebar = () => {
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen)
+
+  if (!isMenuOpen) return null
+
+  return (
+    <div className='w-55 shadow p-2'>
+      <ul className='m-2'>
+        <li className='mt-2 p-2 bg-red-300 rounded-sm'>Home</li>
+        <li className='mt-2 p-2 bg-red-300 rounded-sm'>Shorts</li>
+      </ul>
+      <h1 className='font-bold mx-2'>Subscriptions</h1>
+      <ul className='m-2'>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
