ba561d218633177e80a742b3c770c22c27e15a6a
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2025-12-25 23:28:41 +0530

    Video player

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,23 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../store/appSlice'
 import { useSearchParams } from 'react-router-dom'
+
+const videoByIdUrl = import.meta.env.VITE_LIST_BY_VIDEO_API
+const apiKey = import.meta.env.VITE_YOUTUBE_VIDEO_API
 
 const WatchPage = () => {
   const dispatch = useDispatch()
   const [searchParams] = useSearchParams()
   const videoId = searchParams.get("v")
-
-  console.log(videoId)
+  const [videoDetail, setVideoDetail] = useState(null)
 
   useEffect(() => {
     dispatch(closeMenu())
   }, [])
 
-  
+  const fetchByVideoId = async () => {
+    try {
+      const response = await fetch(`${videoByIdUrl}${videoId}${apiKey}`)
+      const data = await response?.json()
+      console.log(data)
+      setVideoDetail(data.items[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchByVideoId()
+  }, [])
 
   return (
-    <div>WatchPage</div>
+    <div className='p-13'>
+      <iframe
+        width="850"
+        height="500"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className='rounded-lg'
+      />
+      
+    </div>
   )
 }
 
