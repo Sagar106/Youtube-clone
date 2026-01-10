d34254f64579dc4a772af20c57fd08a5c39195b5
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2026-01-10 22:23:57 +0530

    Adding polling to implement live chat feature

--- Youtube-clone/src/components/LiveChat.jsx
+++ Youtube-clone/src/components/LiveChat.jsx
@@ -1,43 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Chat from "./Chat";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../store/chatSlice";
 
 const LiveChat = () => {
+  const chatMessages = useSelector((store) => store.chat.messages);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(
+        addMessage({
+          id: `${Math.random()}-${Date.now()}`,
+          name: "SagarU",
+          message: "Hey! This is a live chat message",
+        })
+      );
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="bg-gray-900 rounded-2xl w-full h-[500px] p-2 text-amber-50">
       <h1 className="text-xl font-semibold p-2">Live Chat</h1>
       <div>
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
-        <Chat
-          name="Sagar Upadhyay"
-          message="Hey! This is a live chat feature"
-        />
+        {chatMessages &&
+          chatMessages.map((chat) => (
+            <Chat key={chat.id} name={chat.name} message={chat.message} />
+          ))}
       </div>
     </div>
   );
