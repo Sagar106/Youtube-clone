087da34bdd72bc0dcef6fbe4dfc39dfe213622cb
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2026-01-29 00:04:16 +0530

    fixed server start issues

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,15 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+});
+
 app.get("/api/search", async (req, res) => {
   const query = req.query.q;
 
   const response = await fetch(
-    `https://serpapi.com/search.json?engine=google_autocomplete&q=${query}&api_key=${process.env.SERP_API_KEY}`
+    `https://serpapi.com/search.json?engine=google_autocomplete&q=${query}&api_key=${process.env.SERP_API_KEY}`,
   );
 
   const data = await response.json();
@@ -22,7 +26,7 @@ app.get("/api/searchVideos", async (req, res) => {
   const searchQuery = req.query.q;
 
   const response = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${searchQuery}&key=${process.env.YOUTUBE_API_KEY}`
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${searchQuery}&key=${process.env.YOUTUBE_API_KEY}`,
   );
 
   const data = await response.json();
@@ -31,11 +35,14 @@ app.get("/api/searchVideos", async (req, res) => {
 
 app.get("/api/getVideoList", async (req, res) => {
   const response = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.YOUTUBE_API_KEY}`
+    `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.YOUTUBE_API_KEY}`,
   );
   const data = await response.json();
 
   res.json(data);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
